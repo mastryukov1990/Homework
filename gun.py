2694ae7a818b157588d2b06824d1377d1fb505be
mastryukov1990 <55547473+mastryukov1990@users.noreply.github.com>
2019-12-21 16:11:04 +0300

    Add files via upload

--- gun.py
+++ gun.py
@@ -14,9 +14,9 @@ canv.pack(fill=tk.BOTH, expand=1)
 
 class ball():
     """Creating bullets for gun"""
-    def __init__(self, x=40, y=450):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x = -100
+        self.y = -100
         self.r = 5
         self.vx = 0
         self.vy = 0
@@ -68,7 +68,8 @@ class gun():
         self.f2_power = 5
         self.f2_on = 0
         self.an = 1
-        self.id = canv.create_line(20, 450, 50, 420, width=7)
+        self.y = 450
+        self.id = canv.create_line(20, self.y, 50, 420, width=7)
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -77,6 +78,8 @@ class gun():
         global balls, bullet
         bullet += 1
         new_ball = ball()
+        new_ball.x = 20
+        new_ball.y = self.y
         new_ball.r += 5
         self.an = math.atan((event.y-new_ball.y) / (event.x-new_ball.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
@@ -87,14 +90,32 @@ class gun():
 
     def targetting(self, event=0):
         if event:
-            self.an = math.atan((event.y-450) / (event.x-20))
+            self.an = math.atan((event.y-self.y) / (event.x-20))
         if self.f2_on:
             canv.itemconfig(self.id, fill='orange')
         else:
             canv.itemconfig(self.id, fill='black')
-        canv.coords(self.id, 20, 450,
+        canv.coords(self.id, 20, self.y,
                     20 + max(self.f2_power, 20) * math.cos(self.an),
-                    450 + max(self.f2_power, 20) * math.sin(self.an)
+                    self.y + max(self.f2_power, 20) * math.sin(self.an)
+                    )
+
+    def move_up(self, event):
+        self.y -= 1
+        if self.y < 10:
+            self.y = 10
+        canv.coords(self.id, 20, self.y,
+                    20 + max(self.f2_power, 20) * math.cos(self.an),
+                    self.y + max(self.f2_power, 20) * math.sin(self.an)
+                    )
+
+    def move_down(self, event):
+        self.y += 1
+        if self.y > 590:
+            self.y = 590
+        canv.coords(self.id, 20, self.y,
+                    20 + max(self.f2_power, 20) * math.cos(self.an),
+                    self.y + max(self.f2_power, 20) * math.sin(self.an)
                     )
 
     def power_up(self):
@@ -177,6 +198,8 @@ def new_game(event=''):
     canv.bind('<Button-1>', g1.fire2_start)
     canv.bind('<ButtonRelease-1>', g1.fire2_end)
     canv.bind('<Motion>', g1.targetting)
+    root.bind('<Up>', g1.move_up)
+    root.bind('<Down>', g1.move_down)
     t1.live = 1
     t2.live = 1
     while t1.live or t2.live:
